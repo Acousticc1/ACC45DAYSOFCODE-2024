acc4906e08e3ced852c72710369a6f0b81df9e7c
Acousticc1 <tomar786rajendra@gmail.com>
2024-10-19 22:09:27 +0530

    Add files via upload

---
+++ day 14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int rad=4;
+    int dia=2*rad;
+    float circ=2*3.14*rad;
+    float area=3.14*rad*rad;
+    printf("Diameter=%d\n",dia);
+    printf("Circumference=%.2f\n",circ);
+    printf("Area=%.2f\n",area);
+}
